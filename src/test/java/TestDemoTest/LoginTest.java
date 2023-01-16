d1454635a4be3332a0a69f24e112f39bf5bcd827
prabhakara BM <prabhakar.mun488@gmail.com>
2023-01-16 16:03:34 +0530

    my first commit

---
+++ src/test/java/TestDemoTest/LoginTest.java
@@ -0,0 +1,12 @@
+package TestDemoTest;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test()
+	public void GenerateOnlineRequest() throws InterruptedException {
+		
+		System.out.println("--------Login is successfull-------");
+		}
+
+}
